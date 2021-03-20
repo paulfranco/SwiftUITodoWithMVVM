8f177efdeb665218d12115a68d4f265ce655e7e0
Paul Franco <mail@paulfran.co>
2021-03-20 03:32:29 -0700

    Initial Commit

---
+++ ListTestProject/ListTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestProjectApp.swift
+//  ListTestProject
+//
+//  Created by Paul Franco on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ListTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
